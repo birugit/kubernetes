15f965b3aa934657bde9360791fdc7de47ad03e2
Swamy Biru <Swamy-Biru@gap.com>
2019-10-23 18:18:16 -0700

    Kubernetes test first commit

---
+++ src/main/java/com/kubernetes/kuberntes/KuberntesApplication.java
@@ -0,0 +1,13 @@
+package com.kubernetes.kuberntes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KuberntesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KuberntesApplication.class, args);
+	}
+
+}
